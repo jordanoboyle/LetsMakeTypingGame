4bc3bc94f09ffac749acacf66973cd4635215866
jordanoboyle <jmcmcode@gmail.com>
2024-11-06 23:06:11 -0700

    end game screen added to timer, dynamic display

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const word             = document.getElementById('word');
 const text             = document.getElementById('text');
 const scoreEl          = document.getElementById('score');
 const timeEl           = document.getElementById('time');
-const endgameEl        = document.getElementById('end-game');
+const endgameEl        = document.getElementById('end-game-container');
 const settingsBtn      = document.getElementById('settings-btn');
 const settings         = document.getElementById('settings');
 const settingsForm     = document.getElementById('settings-form');
@@ -87,7 +87,26 @@ function updateScore() {
 
 //Update the Time
 function updateTime() {
-  console.log(1);
+  time --;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    //END GAME
+    gameOver();
+  }
+}
+
+//GAME OVER, show End Screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time Ran Out</h1>
+    <p>Your Final Score Is: ${score} </p>
+    <button onclick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = 'flex';
 }
 
 updateWordToDOM();
